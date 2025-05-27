d663ada9fe7c4ca17ddc6524b238e112a749816c
Venus <venus.cert@gmail.com>
2025-05-27 13:23:41 +1200

    Implemented PATCH endpoint to update a contact based on its id

--- exercise/src/routes/contacts-routes.js
+++ exercise/src/routes/contacts-routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { retrieveContact, createContact, retrieveContacts } from "../data/contacts-dao.js"
+import { retrieveContact, createContact, retrieveContacts, updateContact } from "../data/contacts-dao.js"
 
 const router = Router();
 
 // Contacts API
+
+// Retrieve a contact based on its ID
 router.get("/:id", async (req, res) => {
     const id = req.params.id;
     const contact = await retrieveContact(id);
@@ -23,6 +25,15 @@ router.delete("/:id", async (req, res) => {
     return res.sendStatus(201).send(`Contact {id} successfully deleted`);
 });
 
+// Updating a contact based on its id
+router.patch("/:id", async (req, res) => {
+    const id = req.params.id;
+    const contact = await retrieveContact(id);
+    const result = await updateContact(id, contact);
+    return res.json(result);
+})
+
+// Create a contact based on what is sent in the body from client
 router.post('/', async (req, res) => {
     // no validation so a crash may occur
     const contactWithID = await createContact(req.body);
